f2ce959a6b8b15619476f0e3c0276fef133b16bb
asadovfazliddin11@gmail.com <asadovfazliddin11@gmail.com>
2021-11-12 10:37:20 +0500

    ft-scroll

--- script.js
+++ script.js
@@ -6,62 +6,46 @@ const mains = document.getElementById("main");
 const qism2 = document.getElementById("qism2");
 const kids = document.getElementById("kids");
 
-menLinks.addEventListener("click",()  => {
-    mains.scrollIntoView({behavior: "smooth"});
+menLinks.addEventListener("click", () => {
+  mains.scrollIntoView({ behavior: "smooth" });
 });
 
-
-
-womenLinks.addEventListener("click",()  => {
-    qism2.scrollIntoView({behavior: "smooth"});
+womenLinks.addEventListener("click", () => {
+  qism2.scrollIntoView({ behavior: "smooth" });
 });
 
-kidsLinks.addEventListener("click",()  => {
-    kids.scrollIntoView({behavior: "smooth"});
+kidsLinks.addEventListener("click", () => {
+  kids.scrollIntoView({ behavior: "smooth" });
 });
-
-
-
-
 
 const buybtn = document.getElementById("buy-now-btn");
 const btns = document.querySelectorAll(".btn");
 const actives = document.querySelector(".active");
 const buy_back = document.getElementById("back-out");
 
-
-function Open () {
-    buybtn.classList.add("active");
+function Open() {
+  buybtn.classList.add("active");
 }
 
 buy_back.addEventListener("click", () => {
-    buybtn.classList.remove("active");
-})
+  buybtn.classList.remove("active");
+});
 
-
-        
-        btns[1].addEventListener("click", Open);
-        btns[2].addEventListener("click", Open);
-        btns[3].addEventListener("click", Open);
-        btns[4].addEventListener("click", Open);
-        btns[5].addEventListener("click", Open);
-        btns[6].addEventListener("click", Open);
-        btns[7].addEventListener("click", Open);
-        btns[8].addEventListener("click", Open);
-
-      
-
+btns[1].addEventListener("click", Open);
+btns[2].addEventListener("click", Open);
+btns[3].addEventListener("click", Open);
+btns[4].addEventListener("click", Open);
+btns[5].addEventListener("click", Open);
+btns[6].addEventListener("click", Open);
+btns[7].addEventListener("click", Open);
+btns[8].addEventListener("click", Open);
 
 const iconc = document.getElementById("nav-menu");
 const ul = document.querySelector(".ul");
 
-
 iconc.addEventListener("click", () => {
-    ul.classList.toggle("active");
+  ul.classList.toggle("active");
 });
-
-
-
 
 const acciconc = document.getElementById("ac-iconc");
 const accaunts = document.getElementById("accaunt");
@@ -69,29 +53,37 @@ const backs = document.getElementById("back");
 const moon0 = document.getElementById("moon");
 const bodys = document.getElementById("body");
 
-
-
-
-
-
-
-acciconc.addEventListener("click", () =>{
-    accaunts.classList.add("active");
-
+acciconc.addEventListener("click", () => {
+  accaunts.classList.add("active");
 });
 
-backs.addEventListener("click", () =>{
-    accaunts.classList.remove("active");
+backs.addEventListener("click", () => {
+  accaunts.classList.remove("active");
 });
 
-moon0.addEventListener("click", () =>{
-    bodys.classList.remove("active");
-})
+moon0.addEventListener("click", () => {
+  bodys.classList.remove("active");
+});
 
+//? scroll part starter
 
+const boxes = document.querySelectorAll(".box1");
+window.addEventListener("scroll", checkBoxes);
 
+checkBoxes();
 
+function checkBoxes() {
+  const triggerButtom = (window.innerHeight / 5) * 4;
 
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
 
+    if (boxTop < triggerButtom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
+}
 
-
+//? scroll part end
